357a5834b9b1ccde9cc15a8fdd0267d756ffd8c0
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-07-03 20:27:40 +0200

    Swagger for all

--- Program.cs
+++ Program.cs
@@ -10,11 +10,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+// Just expose Swagger for now
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // TODO I don't like this, but I also don't like FAILED TO DETERMINE THE HTTPS PORT FOR REDIRECT on a Sunday night
 //app.UseHttpsRedirection();
