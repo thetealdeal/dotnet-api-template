f808d460318083b0d0102d5d9350271015e8f097
MarkEnden <markenden@hotmail.com>
2023-07-06 11:46:42 +0200

    CI/CD first test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "FreezingLOL", "BracingLOL", "ChillyLOL", "CoolLOL", "MildLOL", "WarmLOL", "BalmyLOL", "HotLOL", "SwelteringLOL", "ScorchingLOL"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
